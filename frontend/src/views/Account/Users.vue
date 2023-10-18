fac79f4b875c74b3946a5dc574eebc37c29b859d
zrxpty <streamnik666nik@gmail.com>
2023-10-18 21:47:31 +0700

    qwe

--- frontend/src/views/Account/Users.vue
+++ frontend/src/views/Account/Users.vue
@@ -36,31 +36,7 @@ const users = ref<User[]>([]);
   <style scoped>
   /* Стилизуйте ваш компонент здесь, если это необходимо */
   </style>
-  Подключите компонент к вашему маршрутизатору:
-  vue
-  Copy code
-  // В файле, где у вас настроен маршрутизатор (например, router/index.ts)
-  import { createRouter, createWebHistory } from 'vue-router';
-  import UserList from '@/views/UserList.vue'; // Путь к вашему компоненту UserList.vue
-  
-  const routes = [
-    {
-      path: '/users',
-      name: 'UserList',
-      component: UserList,
-    },
-    // Добавьте другие маршруты, если это необходимо
-  ];
-  
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
-  
-  export default router;
-  В этом примере компонент UserList получает данные о пользователях с вашего бэкенда после монтирования компонента (onMounted), используя функцию fetch. Затем он отображает список пользователей в пользовательском интерфейсе с использованием директивы v-for.
-  
-  Не забудьте настроить путь к вашему бэкенду в URL-адресе запроса (`'ссылка
+ 
   
   
   
